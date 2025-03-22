92662bb6bc35c80a33222f7478b9b3dda62991e4
Lmacroseso <admin@macroseso.com>
2025-03-22 14:42:58 -0400

    Create enzona.php

---
+++ config/enzona.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'apiKey'   => env('ENZONA_API_KEY', 'default-api-key'),
+    'apiSecret'=> env('ENZONA_API_SECRET', 'default-api-secret'),
+    'username' => env('ENZONA_USERNAME', 'default-username'),
+    'password' => env('ENZONA_PASSWORD', 'default-password'),
+    'sandbox'  => env('ENZONA_SANDBOX', true),
+];
