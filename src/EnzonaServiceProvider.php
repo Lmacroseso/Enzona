f20328b59cda042b9dc5e2d7232596daf0a77516
Lmacroseso <admin@macroseso.com>
2025-03-22 18:40:38 -0400

    Create EnzonaServiceProvider.php

---
+++ src/EnzonaServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Lmacroseso\Enzona;
+
+use Illuminate\Support\ServiceProvider;
+
+class EnzonaServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Publicar archivo de configuración
+        $this->publishes([
+            __DIR__.'/../config/enzona.php' => config_path('enzona.php'),
+        ], 'enzona-config'); // El segundo parámetro es el "tag"
+    }
+
+    public function register()
+    {
+        // Aquí puedes registrar otros servicios si es necesario
+    }
+}
