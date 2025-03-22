f8d2b93c6318a12ef66998ec2f0a2c47766ebfd1
Lmacroseso <admin@macroseso.com>
2025-03-22 15:31:17 -0400

    Update Enzona.php

--- src/Enzona.php
+++ src/Enzona.php
@@ -15,14 +15,19 @@ class Enzona
     private $apiKey;
     private $apiSecret;
     private $sandBox;
+    private $usedHost;
 
-    public function __construct($username, $password, $apiKey, $apiSecret, $sandBox)
+    // Constructor que toma las configuraciones desde el archivo config/enzona.php
+    public function __construct()
     {
-        $this->username = env('ENZONA_USERNAME');
-        $this->password = env('ENZONA_PASSWORD');
-        $this->apiKey = env('ENZONA_API_KEY');
-        $this->apiSecret = env('ENZONA_API_SECRET');
-        $this->sandBox = env('ENZONA_SANDBOX', true);
+        $this->username = config('enzona.username');
+        $this->password = config('enzona.password');
+        $this->apiKey = config('enzona.api_key');
+        $this->apiSecret = config('enzona.api_secret');
+        $this->sandBox = config('enzona.sandbox', true);
+
+        // Determina si usar el host de producción o sandbox según la configuración
+        $this->usedHost = ($this->sandBox) ? 'https://sandbox.enzona.com/api/v1/' : 'https://api.enzona.com/api/v1/';
 
         // Se obtiene el token al inicializar la clase
         $this->accessToken = (new RequestToken($this->username, $this->password, $this->apiKey, $this->apiSecret, $this->sandBox))->requestToken();
@@ -31,28 +36,28 @@ class Enzona
     // Método para crear un pago
     public function createPayment($amount, $currency, $paymentMethod)
     {
-        $createPayment = new CreatePayment($this->accessToken);
+        $createPayment = new CreatePayment($this->accessToken, $this->usedHost);
         return $createPayment->processPayment($amount, $currency, $paymentMethod);
     }
 
     // Método para confirmar un pago
     public function confirmPayment($paymentId)
     {
-        $confirmPayment = new ConfirmPayment($this->accessToken);
+        $confirmPayment = new ConfirmPayment($this->accessToken, $this->usedHost);
         return $confirmPayment->processConfirmation($paymentId);
     }
 
     // Método para cancelar un pago
     public function cancelPayment($paymentId)
     {
-        $cancelPayment = new CancelPayment($this->accessToken);
+        $cancelPayment = new CancelPayment($this->accessToken, $this->usedHost);
         return $cancelPayment->processCancellation($paymentId);
     }
 
     // Método para reembolsar un pago
     public function refundPayment($paymentId, $amount)
     {
-        $refundPayment = new RefundPayment($this->accessToken);
+        $refundPayment = new RefundPayment($this->accessToken, $this->usedHost);
         return $refundPayment->processRefund($paymentId, $amount);
     }
 }
