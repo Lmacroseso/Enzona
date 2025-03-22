903dfdb0d3d8d8800e4fe32a784735acdc455fdb
Lmacroseso <admin@macroseso.com>
2025-03-22 15:46:21 -0400

    Update requestTocken.php

--- src/requestTocken.php
+++ src/requestTocken.php
@@ -1,64 +1,63 @@
 <?php
 
-namespace macroseso\enzona;
+namespace Macroseso\Enzona;
+
 use Exception;
 
-
-
-class RequestTocken
+class RequestToken
 {
-    protected $apiKey = '';
-    protected $apiSecret = '';
-    protected $username='';
-    protected $password='';
+    protected $apiKey;
+    protected $apiSecret;
+    protected $username;
+    protected $password;
+    protected $sandBox;
+    protected $usedHost;
+    protected $tokenRoute = "oauth/token";
 
-
-  
-      public function __construct($username, $password, $key, $secret){
-        $this->$apiKey = $key;
+    public function __construct($username, $password, $key, $secret, $sandBox = true)
+    {
+        $this->apiKey = $key;
         $this->apiSecret = $secret;
         $this->username = $username;
         $this->password = $password;
-      }
+        $this->sandBox = $sandBox;
+        $this->usedHost = $this->sandBox ? "https://sandbox.enzona.net/" : "https://api.enzona.net/";
+    }
 
-   public function requestToken()
+    public function requestToken()
     {
-        try{
-          $uri = $this->usedHost . $this->tokenRoute;
-        $param = "grant_type=password&username=$this->username&password=$this->password&scope=enzona_business_payment enzona_business_qr";
-        //$param = array("grant_type" => "password", "username" => "lrodriguez90", "password" => "Macroseso@0619+", "scope" => "enzona_business_payment enzona_business_qr");
+        try {
+            $uri = $this->usedHost . $this->tokenRoute;
+            $param = http_build_query([
+                "grant_type" => "password",
+                "username" => $this->username,
+                "password" => $this->password,
+                "scope" => "enzona_business_payment enzona_business_qr"
+            ]);
 
-        $ch = curl_init($uri);
+            $ch = curl_init($uri);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                "Authorization: Basic " . base64_encode($this->apiKey . ':' . $this->apiSecret),
+                "Content-Type: application/x-www-form-urlencoded"
+            ]);
 
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
-        curl_setopt($ch, CURLOPT_URL, $uri);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Authorization: Basic " . base64_encode($this->apiKey . ':' . $this->apiSecret)
-        ));
-
-        $result = curl_exec($ch);
-        $error = curl_error($ch);
-        $erno = curl_errno($ch);
-        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-        if ($code == 200) {
-            $res = json_decode($result, true);
-            foreach ($res as $key => $value) {
-                $value = $res['access_token'];
-            }
-            $response = $value;
+            $result = curl_exec($ch);
+            $error = curl_error($ch);
+            $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             curl_close($ch);
-            $this->accessToken=$response;
-            return $response;
-        } else {
-            $response = false;
-        }
-        }catch(Exception $e){
-          return   throw new \Exception($e->getMessage());
+
+            if ($code === 200) {
+                $res = json_decode($result, true);
+                return $res['access_token'] ?? false;
+            } else {
+                throw new Exception("Error en la solicitud: Código $code - $error");
+            }
+        } catch (Exception $e) {
+            throw new Exception("Error en requestToken: " . $e->getMessage());
         }
     }
 }
